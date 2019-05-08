ec33ecf24d8c36199c6528ca8ae168d47a37e174
Felix Klauke <info@felix-klauke.de>
2019-05-08 11:30:51 +0200

    Add maven settings and nexus delivery.

---
+++ src/main/java/com/felixklauke/mpt/PinkFluffyUnicorn.java
@@ -0,0 +1,26 @@
+package com.felixklauke.mpt;
+
+public class PinkFluffyUnicorn {
+
+    private final String name;
+    private final int age;
+    private final long cutenessLevel;
+
+    public PinkFluffyUnicorn(String name, int age, long cutenessLevel) {
+        this.name = name;
+        this.age = age;
+        this.cutenessLevel = cutenessLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public long getCutenessLevel() {
+        return cutenessLevel;
+    }
+}
