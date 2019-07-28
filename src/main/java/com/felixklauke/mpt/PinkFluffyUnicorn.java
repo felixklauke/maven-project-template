7791828e44b0c828f668e610bd1b605392364c16
Felix Klauke <info@felix-klauke.de>
2019-07-28 08:58:21 +0200

    Reformat code and add .editorconfig.

--- src/main/java/com/felixklauke/mpt/PinkFluffyUnicorn.java
+++ src/main/java/com/felixklauke/mpt/PinkFluffyUnicorn.java
@@ -2,25 +2,25 @@ package com.felixklauke.mpt;
 
 public class PinkFluffyUnicorn {
 
-    private final String name;
-    private final int age;
-    private final long cutenessLevel;
+  private final String name;
+  private final int age;
+  private final long cutenessLevel;
 
-    public PinkFluffyUnicorn(String name, int age, long cutenessLevel) {
-        this.name = name;
-        this.age = age;
-        this.cutenessLevel = cutenessLevel;
-    }
+  public PinkFluffyUnicorn(String name, int age, long cutenessLevel) {
+    this.name = name;
+    this.age = age;
+    this.cutenessLevel = cutenessLevel;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public int getAge() {
-        return age;
-    }
+  public int getAge() {
+    return age;
+  }
 
-    public long getCutenessLevel() {
-        return cutenessLevel;
-    }
+  public long getCutenessLevel() {
+    return cutenessLevel;
+  }
 }
