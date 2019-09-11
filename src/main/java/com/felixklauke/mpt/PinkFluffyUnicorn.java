f452dc15e6926976d9215c9b3c594fb81909dc1e
Felix Klauke <info@felix-klauke.de>
2019-09-11 15:25:04 +0200

    Add maven checkstyle.

--- src/main/java/com/felixklauke/mpt/PinkFluffyUnicorn.java
+++ src/main/java/com/felixklauke/mpt/PinkFluffyUnicorn.java
@@ -6,6 +6,13 @@ public class PinkFluffyUnicorn {
   private final int age;
   private final long cutenessLevel;
 
+  /**
+   * Create a new pink fluffy unicorn.
+   *
+   * @param name The name of the unicorn.
+   * @param age The age of the unicorn.
+   * @param cutenessLevel The cuteness level of the unicorn.
+   */
   public PinkFluffyUnicorn(String name, int age, long cutenessLevel) {
     this.name = name;
     this.age = age;
